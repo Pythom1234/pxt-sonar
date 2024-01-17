74a39ccdaddd740bee87800a4aec3ed6c1d42417
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-01-17 15:33:38 +0100

    Aktualizovat pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
-basic.forever(function () {
-	
-})
+enum PingUnit {
+    //% block="cm"
+    Centimeters,
+    //% block="μs"
+    MicroSeconds
+}
+//% icon="\uf161" color=#2c3e50
+namespace Sonar {
+    //% block="ping trig %trig|echo %echo|unit %unit"
+    //% block.loc.cs="vzdálenost|ping trig $trig|echo $echo|jednotka $unit"
+    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
+
+        // read pulse
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+
+        switch (unit) {
+            case PingUnit.Centimeters: return d / 58;
+            default: return d;
+        }
+    }
+}
